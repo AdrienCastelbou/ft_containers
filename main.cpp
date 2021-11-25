38dbbd8844be802006b5fc7fb12c9ab2dffb210a
Adrien CASTELBOU <acastelb@e1r6p1.42.fr>
2021-11-25 10:19:56 +0100

    start to write map class w/ member types & var

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 
 int main () {
-  ft::pair <int,int> foo;
+/*  ft::pair <int,int> foo;
   ft::pair <int,int> bar;
 
   foo = ft::make_pair (3,20);
@@ -26,14 +26,14 @@ int main () {
   //std::cout << std::endl << "////" << std::endl;
   c = new ft::BST<int, int>(ft::make_pair (1, 3));
   start->insert(c, start);
-  /*c = new ft::BST<int, int>(ft::make_pair (11, 3));
+  c = new ft::BST<int, int>(ft::make_pair (11, 3));
   start->insert(c, start);
   c = new ft::BST<int, int>(ft::make_pair (16, 3));
   start->insert(c, start);
   c = new ft::BST<int, int>(ft::make_pair (15, 3));
-  start->insert(c, start);*/
+  start->insert(c, start);
   //start->right_rotation(&start->search(12), &start);
-  start->show();
+  start->show();*/
 
   return 0;
 }
